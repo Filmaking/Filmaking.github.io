735e540b4df574f6312dd116c86e063d2c426efe
franciscofofo <39025735+franciscofofo@users.noreply.github.com>
2019-05-17 12:56:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+$(document).ready(() => {
+  $(window).on('scroll', () => {
+    let wScroll = $(this).scrollTop();
+    $('.parallax').css({
+      'transform': 'translate(0, ' + wScroll / 2 + 'px)'
+    })
+  })
+})
